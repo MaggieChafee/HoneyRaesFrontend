d5c34e466c2add7877ab4c60947160711b119905
MaggieChafee <maggie.chafee@gmail.com>
2024-02-03 13:52:57 -0600

    create somewhat working

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,3 +1,107 @@
+import { useEffect, useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { createServiceTicket } from "../../data/serviceTicketsData";
+import { getCustomers } from "../../data/customerData";
+
+const initialState = {
+  id: 0,
+  customerId: 0,
+  employeeId: 0,
+  description: '',
+  emergency: false,
+  dateCompleted: null,
+}
+
 export default function CreateTicket() {
-  return <h3>Submit a Ticket</h3>;
+  const [formInput, setFormInput] = useState({initialState});
+  const [employees, setEmployees] = useState([]);
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    getCustomers.then(setCustomers);
+  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const payload = { 
+      ...formInput
+    };
+    createServiceTicket(payload);
+  }
+
+  return <>
+    <h3>Submit a Ticket</h3>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label for="customerId">
+            Customer
+          </Label>
+          <Input
+            id="customerId"
+            name="customerId"
+            value={formInput.customerId}
+            type="select"
+          >{customers.map((customer) => (
+            <option key={customer.id} value={customer.id}>{customer.name}</option>
+            ))}
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">
+            Describe the issue. 
+          </Label>
+          <Input
+            id="exampleText"
+            name="description"
+            value={formInput.description}
+            type="textarea"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="employeeId">
+            Employee
+          </Label>
+          <Input
+            id="employeeId"
+            name="employeeId"
+            value={formInput.employeeId}
+            type="select"
+          >
+            <option>
+             87
+            </option>
+            <option>
+             77
+            </option>
+            <option>
+              90
+            </option>
+          </Input>
+        </FormGroup>
+        <FormGroup check>
+          <Label check for="emergency">
+            This is an emergency.
+          </Label>
+          <Input id="emergency" type="checkbox" name="emergency" checked={formInput.emergency} />
+          
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleDate">
+            DateCompleted
+          </Label>
+          <Input
+            id="exampleDate"
+            name="date"
+            value-={formInput.dateCompleted}
+            placeholder="date placeholder"
+            type="date"
+          />
+        </FormGroup>
+        <Button
+        color="primary"
+        tag="input"
+        type="submit"
+        />
+      </Form>
+  </>  
 }
--- src/data/serviceTicketsData.js
+++ src/data/serviceTicketsData.js
@@ -7,3 +7,24 @@ export const getServiceTickets = () => {
 export const getSingleServiceTicket = (id) => {
   return fetch(`${_apiUrl}/${id}`).then((r) => r.json());
 }
+
+export const deleteServiceTicket = (id) => new Promise((resolve, reject) => {
+  fetch(`${_apiUrl}/delete/${id}`), {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }.then((data) => resolve(data))
+     .catch(reject)
+  };
+})
+
+export const createServiceTicket = (payload) => new Promise((resolve, reject) => {
+  fetch(_apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }, 
+    body: JSON.stringify(payload)
+  }).then((data) => resolve(data))
+    .catch(reject)
+});
